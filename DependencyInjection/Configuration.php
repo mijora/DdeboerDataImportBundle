c4c60df6a7eaa2d84f1c965460aff837e3f0e366
Rimvydas <lithstud@gmail.com>
2025-01-21 17:02:40 +0200

    Update Configuration.php for Symfony 4.*

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ddeboer_data_import');
+        $treeBuilder = new TreeBuilder('ddeboer_data_import');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
